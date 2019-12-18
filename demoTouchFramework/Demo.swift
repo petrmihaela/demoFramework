ba218c3a7274d1a883c73a65afbab45f754ff982
Petrisor Mihaela <petr.mihaela97@gmail.com>
2019-12-18 13:54:06 +0200

    first commit

---
+++ demoTouchFramework/Demo.swift
@@ -0,0 +1,24 @@
+//
+//  Demo.swift
+//  demoTouchFramework
+//
+//  Created by HPM on 18/12/2019.
+//  Copyright © 2019 HPM. All rights reserved.
+//
+
+import Foundation
+
+public class Demo: NSObject {
+    
+    
+    private override init() {
+        super.init()
+    }
+    
+    public class func yourName(name: String) {
+        print("**************")
+        print(name)
+        print("**************")
+
+    }
+}
